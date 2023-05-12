ad27f811ba414dcd0263e24e006910d0b1dc299e
D3Xect <114375108+D3Xect@users.noreply.github.com>
2023-05-12 18:39:01 +0700

    Add files via upload

---
+++ AIM0606D2203 Deploying Natural Language Processing/app/static/js/aplikasi.js
@@ -0,0 +1,39 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// let box = document.querySelector('.box');
+
+// let audioArr = document.getElementsByTagName('audio');
+
+// console.log(audioArr);
+
+// box.addEventListener('mouseenter', ()=>{
+//   audioArr[0].play()
+// });
+
+// box.addEventListener('mouseleave', ()=>{
+//   audioArr[1].play()
+// });
